0ca619230c0ebf65b1e52f5b9e51be8dcb9cabec
smokeless <ross.russell.ca@gmail.com>
2018-03-13 18:06:59 -0700

    added cache

--- rod_equip.py
+++ rod_equip.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
 import re
+import ast
 
 '''
 This currently only works with rodpedia, the file must be named items and in the same
@@ -12,19 +14,22 @@ todo:
 add support for selecting a file from the command line.
 add support for searching for items that don't immediately pop on rodpedia
 add threading to speed up the searching
-create a local database for searching before hitting rodpedia
 '''
 
 
-def get_item_info(item:str)->dict:
+
+def get_web_item_info(item:str)->dict:
     r = requests.get('https://rodpedia.realmsofdespair.info/index.php?title=Special%3ASearch&search={0}&go=Go'.format(item))
     print('Looking up {0}......'.format(item))
     soup = BeautifulSoup(r.text, 'html.parser')
     raw = soup.find_all('pre')
     info = ''
-    for i in raw:
-        info += i.text
-    return {item:info}
+    if not raw:
+        info = 'Item not found.\n'
+    else:
+        for i in raw:
+            info += i.text
+    return {item: info}
 
 def format_item_list(items:list)->list:
     '''
@@ -36,14 +41,13 @@ def format_item_list(items:list)->list:
     for i in items:
         newItem = re.sub("[\(\[].*?[\)\]]", "", i)
         newItem = newItem.strip()
-        newItem = newItem.capitalize()
+        newItem = newItem.lower()
         cleanList.append(newItem)
     return cleanList
 
 def create_item_list(fileName:str):
-    fileName = 'items'
     try:
-        with open('items', 'r') as f:
+        with open(fileName, 'r') as f:
             itemList = f.read().split('\n')
             itemList = [i.strip() for i in itemList]
             return itemList
@@ -53,25 +57,85 @@ def create_item_list(fileName:str):
 
 def present_info(contents: dict):
     for k,v in contents.items():
-       if v == '':
-           print(k)
-           print('Unable to locate item.')
-       else:
+        if v == '':
+            print(k)
+            print('Present info unable to locate item.')
+            print('')
+        else:
             print(k)
             print(v)
+
+
+def write_dict(itemsDict:dict):
+    with open('./data/database.dict', 'w') as f:
+        f.write(str(itemsDict))
+
+
+def read_dict():
+    try:
+        with open('./data/database.dict', 'r') as f:
+                currentDB = ast.literal_eval(f.read())
+                return currentDB
+    except FileNotFoundError:
+        print('No dict file exists.')
+
+
+def database_has_data()->bool:
+    try:
+        with open('./data/database.dict', 'r') as f:
+            f.seek(0)  # ensure you're at the start of the file..
+            first_char = f.read(1)
+            if not first_char:
+                return False
+            else:
+                return True
+    except FileNotFoundError:
+        print('No dict file exists.')
+
+
+def get_database_items():
+    if database_has_data():
+        return read_dict()
+    else:
+        return {}
 
 
 # Main function to parse stuff.
 
 if __name__ == "__main__":
+
     itemList = create_item_list('items')
     itemList = format_item_list(itemList)
-    itemsDict = {}
+
     print('{0} items to be searched.'.format(len(itemList)))
     counter = len(itemList)
+
+    # Check our dict file first.
+    cachedInfo = get_database_items()
+    selectedInfo = {}
+    newItemList = []
+
+    print('Checking cache...')
     for i in itemList:
-        info = get_item_info(i)
-        itemsDict.update(info)
-        counter -= 1
-        print('{0} items remain.'.format(counter))
-    present_info(itemsDict)
+        if i in cachedInfo.keys():
+            selectedInfo[i] = cachedInfo[i]
+            counter -= 1
+        else:
+            newItemList.append(i)
+    print('{0} items remain.\n'.format(counter))
+
+    itemsDict = {}
+    if not newItemList:
+        present_info(selectedInfo)
+        exit(0)
+    else:
+        for i in newItemList:
+            info = get_web_item_info(i)
+            itemsDict.update(info)
+            counter -= 1
+            print('{0} web checks remain.'.format(counter))
+
+        itemsDict.update(cachedInfo)
+        present_info(itemsDict)
+        write_dict(itemsDict)
+        exit(0)
